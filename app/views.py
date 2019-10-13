6cdf4736a4294e1532328d810a5b084baa389983
John-5014 <kaiserjohn52@gmail.com>
2019-10-13 22:13:53 +0300

    add code to get news sources from  category of news

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 
@@ -11,9 +12,15 @@ def index():
   View root page that returns the index page and its data
   '''
 
+  # Getting news articles
+
+  articles_news = get_news('articles')
+  print(articles_news)
+
+
   title = 'Home - Welcome to the best news app'
 
-  return render_template('index.html',title = title)
+  return render_template('index.html',title = title,articles = articles_news)
 
 
 @app.route('/news/<int:news_id>')
