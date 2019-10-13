548ad4ab7f448a19238a685869b09ddd855f7466
John-5014 <kaiserjohn52@gmail.com>
2019-10-13 14:24:22 +0300

    add new route for news() view function and int to transform content to integer

---
+++ app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+# Views
+
+@app.route('/')
+def index():
+
+  '''
+
+  View root page that returns the index page and its data
+  '''
+
+  message = 'Hello World'
+
+  return render_template('index.html',message = message)
+
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+  '''
+  View news page function that returns the movie details page and its data
+  '''
+
+  return render_template('news.html',id = news_id)
